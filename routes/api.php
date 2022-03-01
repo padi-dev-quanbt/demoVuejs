732c5edd3920bcb7092cfa62774d16ccaa979aee
DESKTOP-42CBIIG\DELL1 <quanbt@paditech.com>
2022-03-01 15:59:55 +0700

    no message

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,27 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/products', function () {
+    return response()->json([
+        "error_code" => 0,
+        "message" => "success",
+        "data" => [
+            [
+                "id" => 1001,
+                "name" => "sản phẩm 1001",
+                "price" => 500000
+            ],
+            [
+                "id" => 1002,
+                "name" => "sản phẩm 1002",
+                "price" => 5000000
+            ],
+            [
+                "id" => 1003,
+                "name" => "sản phẩm 1003",
+                "price" => 1500000
+            ]
+        ]
+    ]);
+});
